92d62a08c957d8e7281af06e664375c0ea4ffbb2
yue_zhou <yue_zhou@intsig.net>
2019-05-09 17:12:43 +0800

    password box

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Item
+    {
+        public string BatchName { get; set; }
+        public int Number { get; set; }
+
+        public override string ToString()
+        {
+            return $"{BatchName}--{Number}";
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new List<Item>();
+            list.Add(new Item { BatchName = "BOne", Number = 10 });
+            list.Add(new Item { BatchName = "BTwo", Number = 3 });
+            list.Add(new Item { BatchName = "BOne", Number = 3 });
+            list.Add(new Item { BatchName = "BOne", Number = 4 });
+            list.Add(new Item { BatchName = "BTwo", Number = 5 });
+            list.Add(new Item { BatchName = "BOne", Number = 5 });
+            list.Add(new Item { BatchName = "BOne", Number = 7 });
+            list.Add(new Item { BatchName = "BTwo", Number = 8 });
+            list.Add(new Item { BatchName = "B", Number = 8 });
+
+            var groups = from i in list
+                         group i by i.BatchName into g
+                         orderby g.Key
+                         select g;
+            var batches = new List<List<Item>>();
+            foreach(var g in groups)
+            {
+                var g_ordered = from i in g orderby i.Number select i;
+                batches.Add(g_ordered.ToList());
+            }
+
+            batches.SelectMany(i => i)
+                .ToList()
+                .ForEach(i => Console.WriteLine(i));
+
+            Console.WriteLine("done");
+            Console.ReadKey();
+        }
+    }
+}
--- dg_trial/ISPasswordBox.cs
+++ dg_trial/ISPasswordBox.cs
@@ -1,62 +1,11 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Media;
 
 namespace dg_trial
 {
-    public class ISPasswordBox : FrameworkElement
+    class ISPasswordBox
     {
-        private PasswordBox passwordBox = new PasswordBox();
-
-        public ISPasswordBox()
-        {
-            this.passwordBox.PasswordChanged += PasswordBox_PasswordChanged;
-            this.passwordBox.HorizontalAlignment = HorizontalAlignment.Stretch;
-        }
-
-        private void PasswordBox_PasswordChanged(object sender, RoutedEventArgs e)
-        {
-            this.Password = passwordBox.Password;
-        }
-
-
-
-        public string Password
-        {
-            get { return (string)GetValue(PasswordProperty); }
-            set { SetValue(PasswordProperty, value); }
-        }
-
-        // Using a DependencyProperty as the backing store for Password.  This enables animation, styling, binding, etc...
-        public static readonly DependencyProperty PasswordProperty =
-            DependencyProperty.Register("Password", typeof(string), typeof(ISPasswordBox), new PropertyMetadata(null));
-
-
-
-
-        protected override IEnumerator LogicalChildren => new[] { passwordBox }.GetEnumerator();
-
-        protected override int VisualChildrenCount => 1;
-        protected override Visual GetVisualChild(int index)
-        {
-            return passwordBox;
-        }
-
-        protected override Size MeasureOverride(Size availableSize)
-        {
-            passwordBox.Measure(availableSize);
-            return passwordBox.DesiredSize;
-        }
-
-        protected override Size ArrangeOverride(Size finalSize)
-        {
-            passwordBox.Arrange(new Rect(0, 0, finalSize.Width, finalSize.Height));
-            return finalSize;
-        }
     }
 }
